1529044a99d2d93974e820ab2c1162c44a326b64
KennYZuniga <49075800+KennYZuniga@users.noreply.github.com>
2019-03-29 13:01:54 -0500

    Interfaz de Curso
    Este modulo del proyecto indica a la interfaz de curso en la plataforma easylito.

---
+++ EasylitoEnCurso/database/script.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE php_mysql_crud;
+
+use php_mysql_crud;
+
+CREATE TABLE task(
+  id INT(11) PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+DESCRIBE task;
+
+
+
+CREATE TABLE foro1(
+  id INT(11) PRIMARY KEY AUTO_INCREMENT,
+  titulo VARCHAR(255) NOT NULL,
+  contenido TEXT,
+  fechaDeCreacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+DESCRIBE foro1;
